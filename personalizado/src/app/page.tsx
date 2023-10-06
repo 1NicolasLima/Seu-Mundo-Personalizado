d6e9b57fb13acb0a2cca4bda78df0c2137035eec
Nickz <nicolasaraujo845@gmail.com>
2023-10-06 08:53:39 -0300

    adicionado o layout da taskbar

--- personalizado/src/app/page.tsx
+++ personalizado/src/app/page.tsx
@@ -1,13 +1,21 @@
 // `app/page.tsx` is the UI for the `/` URL
 import Link from "next/link"
 import './globals.css'
-import Example from "@/components/taskbar/TaskBar"
+import Image from 'next/image'
+import wallpaper from '../../public/img-princ.png'
 
 export default function Page() {
     return (
     <div>
-      <Example/>
       <Link href="/home">Home</Link>
+      <Image
+          src={wallpaper}
+          alt="Wallpaper do site"
+          // width={500} automatically provided
+          // height={500} automatically provided
+          // blurDataURL="data:..." automatically provided
+          // placeholder="blur" // Optional blur-up while loading
+        />
     </div>
     
     )
