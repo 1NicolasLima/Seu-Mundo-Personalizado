10c3839ff6d870de14733707a93e036ea1cc212a
Nickz <nicolasaraujo845@gmail.com>
2023-10-06 16:59:41 -0300

    estilizacao dos produtos 1.0

--- personalizado/src/app/page.tsx
+++ personalizado/src/app/page.tsx
@@ -3,6 +3,9 @@ import Link from "next/link"
 import './globals.css'
 import Image from 'next/image'
 import wallpaper from '../../public/img-princ.png'
+import Cadeira from '../../public/images/cadeira.png'
+import qualificacao from '../../public/images/qualificacao.svg'
+import CoracaoVermelho from '../../public/images/corao-like.svg'
 
 export default function Page() {
     return (
@@ -21,17 +24,76 @@ export default function Page() {
           <h2 className="font-bold font-['Poppins'] text-3xl">Ofertas especiais</h2>
           <p className="font-['Poppins'] text-xl">Os melhores preços</p>
         </div>
-        <div className="flex justify-center">
-          <div className="grid grid-cols-4 gap-2 justify-items-center mt-20 w-3/4">
-            <div className="w-72 h-52 border-2 rounded-md">01</div>
-            <div className="w-72 h-52 border-2 rounded-md">02</div>
-            <div className="w-72 h-52 border-2 rounded-md">03</div>
-            <div className="w-72 h-52 border-2 rounded-md">04</div>
-            <div className="w-72 h-52 border-2 rounded-md">05</div>
-            <div className="w-72 h-52 border-2 rounded-md">06</div>
-            <div className="w-72 h-52 border-2 rounded-md">07</div>
-            <div className="w-72 h-52 border-2 rounded-md">08</div>
-            <div className="w-72 h-52 border-2 rounded-md">09</div>
+        <div className="flex justify-center ml-32 mr-32">
+          <div className="grid grid-cols-4 gap-2 justify-items-center mt-5 w-3/4 ">
+            <div className="w-64 mb-10 h-56 rounded-xl shadow-lg shadow-gray-300 md:shadow-xl md:shadow-gray-300 flex flex-col">
+              <Image className="h-max self-center" src={Cadeira} alt="Produto 1"/>
+              <h1 className="ml-3 mt-2 font-bold font-['Poppins'] text-xl">Geladeira Brastemp</h1>
+              <Image className="ml-4 w-14  h-max font- ['Poppins'] text-md" src={qualificacao} alt="qualificacao do produto"/>
+              <div className="flex justify-between mr-10">
+                <h2 className="ml-3 mt-2">R$ 3.599<span className="text-xs">,99</span></h2>
+                <button className="">
+                  <Image className="w-8" src={CoracaoVermelho} alt="Cutiu"/>
+                </button> 
+              </div>
+                <span className="ml-3 text-xs font-['Poppins'] text-current">10x de R$250</span>
+            </div>
+            <div className="w-64 h-56 rounded-xl shadow-lg shadow-gray-300 md:shadow-xl md:shadow-gray-300 flex flex-col">
+              <Image className="h-max self-center" src={Cadeira} alt="Produto 1"/>
+              <h1 className="ml-3 mt-2 font-bold font-['Poppins'] text-xl">Geladeira Brastemp</h1>
+              <Image className="ml-4 w-14  h-max font- ['Poppins'] text-md" src={qualificacao} alt="qualificacao do produto"/>
+              <h2 className="ml-3 mt-2">R$ 3.599<span className="text-xs">,99</span></h2> 
+              <span className="ml-3 text-xs font-['Poppins'] text-current">10x de R$250</span>
+            </div>
+            <div className="w-64 h-56 rounded-xl shadow-lg shadow-gray-300 md:shadow-xl md:shadow-gray-300 flex flex-col">
+              <Image className="h-max self-center" src={Cadeira} alt="Produto 1"/>
+              <h1 className="ml-3 mt-2 font-bold font-['Poppins'] text-xl">Geladeira Brastemp</h1>
+              <Image className="ml-4 w-14  h-max font- ['Poppins'] text-md" src={qualificacao} alt="qualificacao do produto"/>
+              <h2 className="ml-3 mt-2">R$ 3.599<span className="text-xs">,99</span></h2> 
+              <span className="ml-3 text-xs font-['Poppins'] text-current">10x de R$250</span>
+            </div>
+            <div className="w-64 h-56 rounded-xl shadow-lg shadow-gray-300 md:shadow-xl md:shadow-gray-300 flex flex-col">
+              <Image className="h-max self-center" src={Cadeira} alt="Produto 1"/>
+              <h1 className="ml-3 mt-2 font-bold font-['Poppins'] text-xl">Geladeira Brastemp</h1>
+              <Image className="ml-4 w-14  h-max font- ['Poppins'] text-md" src={qualificacao} alt="qualificacao do produto"/>
+              <h2 className="ml-3 mt-2">R$ 3.599<span className="text-xs">,99</span></h2> 
+              <span className="ml-3 text-xs font-['Poppins'] text-current">10x de R$250</span>
+            </div>
+            <div className="mb-10 w-64 h-56 rounded-xl shadow-lg shadow-gray-300 md:shadow-xl md:shadow-gray-300 flex flex-col">
+              <Image className="h-max self-center" src={Cadeira} alt="Produto 1"/>
+              <h1 className="ml-3 mt-2 font-bold font-['Poppins'] text-xl">Geladeira Brastemp</h1>
+              <Image className="ml-4 w-14  h-max font- ['Poppins'] text-md" src={qualificacao} alt="qualificacao do produto"/>
+              <h2 className="ml-3 mt-2">R$ 3.599<span className="text-xs">,99</span></h2> 
+              <span className="ml-3 text-xs font-['Poppins'] text-current">10x de R$250</span>
+            </div>
+            <div className="w-64 h-56 rounded-xl shadow-lg shadow-gray-300 md:shadow-xl md:shadow-gray-300 flex flex-col">
+              <Image className="h-max self-center" src={Cadeira} alt="Produto 1"/>
+              <h1 className="ml-3 mt-2 font-bold font-['Poppins'] text-xl">Geladeira Brastemp</h1>
+              <Image className="ml-4 w-14  h-max font- ['Poppins'] text-md" src={qualificacao} alt="qualificacao do produto"/>
+              <h2 className="ml-3 mt-2">R$ 3.599<span className="text-xs">,99</span></h2> 
+              <span className="ml-3 text-xs font-['Poppins'] text-current">10x de R$250</span>
+            </div>
+            <div className="w-64 h-56 rounded-xl shadow-lg shadow-gray-300 md:shadow-xl md:shadow-gray-300 flex flex-col">
+              <Image className="h-max self-center" src={Cadeira} alt="Produto 1"/>
+              <h1 className="ml-3 mt-2 font-bold font-['Poppins'] text-xl">Geladeira Brastemp</h1>
+              <Image className="ml-4 w-14  h-max font- ['Poppins'] text-md" src={qualificacao} alt="qualificacao do produto"/>
+              <h2 className="ml-3 mt-2">R$ 3.599<span className="text-xs">,99</span></h2> 
+              <span className="ml-3 text-xs font-['Poppins'] text-current">10x de R$250</span>
+            </div>
+            <div className="w-64 h-56 rounded-xl shadow-lg shadow-gray-300 md:shadow-xl md:shadow-gray-300 flex flex-col">
+              <Image className="h-max self-center" src={Cadeira} alt="Produto 1"/>
+              <h1 className="ml-3 mt-2 font-bold font-['Poppins'] text-xl">Geladeira Brastemp</h1>
+              <Image className="ml-4 w-14  h-max font- ['Poppins'] text-md" src={qualificacao} alt="qualificacao do produto"/>
+              <h2 className="ml-3 mt-2">R$ 3.599<span className="text-xs">,99</span></h2> 
+              <span className="ml-3 text-xs font-['Poppins'] text-current">10x de R$250</span>
+            </div>
+            <div className="w-64 h-56 rounded-xl shadow-lg shadow-gray-300 md:shadow-xl md:shadow-gray-300 flex flex-col">
+              <Image className="h-max self-center" src={Cadeira} alt="Produto 1"/>
+              <h1 className="ml-3 mt-2 font-bold font-['Poppins'] text-xl">Geladeira Brastemp</h1>
+              <Image className="ml-4 w-14  h-max font- ['Poppins'] text-md" src={qualificacao} alt="qualificacao do produto"/>
+              <h2 className="ml-3 mt-2">R$ 3.599<span className="text-xs">,99</span></h2> 
+              <span className="ml-3 text-xs font-['Poppins'] text-current">10x de R$250</span>
+            </div>
           </div>
         </div>
         
