f74887d16fe2c360f649998cf9ecfec38adb0812
Nickz <nicolasaraujo845@gmail.com>
2023-10-06 15:37:54 -0300

    estilizacao do grid

--- personalizado/src/app/layout.tsx
+++ personalizado/src/app/layout.tsx
@@ -35,20 +35,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-      <Disclosure as="nav" className="bg-gray-800">
+      <Disclosure as="nav" className="bg-teal-700">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-teal-900 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="absolute -inset-0.5" />
                   <span className="sr-only">Open main menu</span>
-                  {/* {open ? (
+                  {open ? (
                     <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
                   ) : (
                     <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                  )} */}
+                  )}
                 </Disclosure.Button>
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
@@ -67,7 +67,7 @@ export default function RootLayout({
                         key={item.name}
                         href={item.href}
                         className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          item.current ? 'bg-teal-900 text-white' : 'text-gray-300 hover:bg-teal-900 hover:text-white',
                           'rounded-md px-3 py-2 text-sm font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
@@ -81,7 +81,7 @@ export default function RootLayout({
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <button
                   type="button"
-                  className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                  className="relative rounded-full bg-teal-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                 >
                   <span className="absolute -inset-1.5" />
                   <span className="sr-only">View notifications</span>
@@ -95,7 +95,7 @@ export default function RootLayout({
                 </button>
                 <Menu as="div" className="relative ml-3">
                   <div>
-                    <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    <Menu.Button className="relative flex rounded-full bg-teal-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="absolute -inset-1.5" />
                       <span className="sr-only">Open user menu</span>
                       <img
@@ -119,7 +119,7 @@ export default function RootLayout({
                         {({ active }) => (
                           <a
                             href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                            className={classNames(active ? 'bg-teal-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
                             Your Profile
                           </a>
@@ -129,7 +129,7 @@ export default function RootLayout({
                         {({ active }) => (
                           <a
                             href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                            className={classNames(active ? 'bg-teal-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
                             Settings
                           </a>
@@ -139,7 +139,7 @@ export default function RootLayout({
                         {({ active }) => (
                           <a
                             href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                            className={classNames(active ? 'bg-teal-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
                             Sign out
                           </a>
@@ -159,7 +159,7 @@ export default function RootLayout({
                   as="a"
                   href={item.href}
                   className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    item.current ? 'bg-teal-900 text-white' : 'text-gray-300 hover:bg-teal-700 hover:text-white',
                     'block rounded-md px-3 py-2 text-base font-medium'
                   )}
                   aria-current={item.current ? 'page' : undefined}
--- personalizado/src/app/page.tsx
+++ personalizado/src/app/page.tsx
@@ -6,17 +6,36 @@ import wallpaper from '../../public/img-princ.png'
 
 export default function Page() {
     return (
-    <div>
-      <Link href="/home">Home</Link>
-      <Image
-          src={wallpaper}
-          alt="Wallpaper do site"
-          // width={500} automatically provided
-          // height={500} automatically provided
-          // blurDataURL="data:..." automatically provided
-          // placeholder="blur" // Optional blur-up while loading
-        />
-    </div>
-    
+      <>
+        <div className="flex items-center justify-center bg-slate-900">
+          <Image className=""
+              src={wallpaper}
+              alt="Wallpaper do site"
+              // width={500} automatically provided
+              // height={500} automatically provided
+              // blurDataURL="data:..." automatically provided
+              // placeholder="blur" // Optional blur-up while loading
+            />
+        </div>
+        <div className="grid justify-center">
+          <h2 className="font-bold font-['Poppins'] text-3xl">Ofertas especiais</h2>
+          <p className="font-['Poppins'] text-xl">Os melhores preços</p>
+        </div>
+        <div className="flex justify-center">
+          <div className="grid grid-cols-4 gap-2 justify-items-center mt-20 w-3/4">
+            <div className="w-72 h-52 border-2 rounded-md">01</div>
+            <div className="w-72 h-52 border-2 rounded-md">02</div>
+            <div className="w-72 h-52 border-2 rounded-md">03</div>
+            <div className="w-72 h-52 border-2 rounded-md">04</div>
+            <div className="w-72 h-52 border-2 rounded-md">05</div>
+            <div className="w-72 h-52 border-2 rounded-md">06</div>
+            <div className="w-72 h-52 border-2 rounded-md">07</div>
+            <div className="w-72 h-52 border-2 rounded-md">08</div>
+            <div className="w-72 h-52 border-2 rounded-md">09</div>
+          </div>
+        </div>
+        
+        <Link className="m-20" href="/home">Home</Link>
+    </>
     )
   }
